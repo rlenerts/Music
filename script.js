20546c29226385ae57d99de77408e21c42b3d617
rlenerts <114050583+rlenerts@users.noreply.github.com>
2023-05-10 09:35:43 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const coolbutton = document.querySelector('.js-music');
+
+console.log(coolbutton)
+
+coolbutton.addEventListener('click', () => {
+  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley', '_blank');
+})
